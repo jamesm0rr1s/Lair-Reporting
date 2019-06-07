8777c81e5f3ab2c604c0da8ee9adda85b2a91095
jamesm0rr1s <34630328+jamesm0rr1s@users.noreply.github.com>
2019-06-07 09:27:25 -0400

    Add files via upload

---
+++ Lair-json-to-csv.ps1
@@ -0,0 +1,60 @@
+﻿# Set the file path and name
+$filepath = (pwd).Path
+$filename = "lair.json"
+
+# Get the Lair json export
+$jsonFile = ((Get-Content -Raw -Path "$filepath\$filename") -replace ',\n            "webdirectories": \[\],',',') -replace ',\n    "authinterfaces": \[\],',',' | ConvertFrom-Json
+
+# Create empty lists
+$ports = @()
+$hosts = @()
+
+# Loop through all of the hosts in the json file
+foreach ($lairHost in $jsonFile.hosts){
+
+    # Loop through all of the services
+    foreach ($service in $lairHost.services){
+
+        # Add the port to the port list
+        $ports += $service.port
+    }
+}
+
+# Get the unique ports
+$ports = $ports | Select-Object -Unique | Sort-Object
+
+# Loop through all of the hosts in the json file
+foreach ($lairHost in $jsonFile.hosts){
+
+    # Create a temporary object
+    $newHost = New-Object PSObject
+
+    # Add the IP address
+    Add-Member -InputObject $newHost -MemberType NoteProperty -Name IP -Value $lairHost.ipv4
+
+    # Loop through all of the ports
+    foreach ($port in $ports){
+
+        # Add field for port and service
+        Add-Member -InputObject $newHost -MemberType NoteProperty -Name $port -Value ""
+        Add-Member -InputObject $newHost -MemberType NoteProperty -Name "$port Service" -Value ""
+    }
+
+    # Loop through all of the services
+    foreach ($service in $lairHost.services){
+
+        # Set the port and product
+        $port = [string]$service.port
+        $product = [string]$service.product
+
+        # Add the port and product
+        $newHost.($port) = $port
+        $newHost.("$port Service") = $product
+    }
+
+    # Add the new host to the list
+    $hosts += $newHost
+}
+
+# Export results to a csv file
+$hosts | Export-Csv -Path "$filepath\Lair.csv" -NoTypeInformation
---
+++ README.md
@@ -0,0 +1,17 @@
+# Lair Reporting
+
+Lair Reporting is a penetration testing and red teaming script that converts Lair JSON exports to CSV files. This script was tested using nmap exports from Lair.
+
+## Installation
+
+Clone the GitHub repository
+```
+git clone https://github.com/jamesm0rr1s/Lair-Reporting /opt/jamesm0rr1s/Lair-Reporting
+```
+
+## Usage
+
+Run the file below to convert a Lair export titled "lair.json" to "lair.csv":
+```
+Lair-json-to-csv.ps1
+```
